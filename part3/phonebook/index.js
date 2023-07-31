dbabe830c9eb1d810e00313fd51f10eab5c77c74
Pak Yu Tom <pakyutom@Paks-MacBook-Air.local>
2023-07-31 16:46:54 +0800

    Exercise3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('data', (request, response) => {return request.method === 'POST' ? JSON.stringify(request.body) : ''})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data :date[iso]'))
 
 let persons = [
     { 
