99d321863ebc162bd98f3feea7da1518a33c2560
Pak Yu Tom <pakyutom@Paks-MacBook-Air.local>
2023-08-02 16:08:52 +0800

    Exercise 3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,34 +6,34 @@ const Person = require('./models/person')
 const app = express()
 app.use(cors())
 app.use(express.json())
-morgan.token('data', (request, response) => {return request.method === 'POST' ? JSON.stringify(request.body) : ''})
+morgan.token('data', (request) => {return request.method === 'POST' ? JSON.stringify(request.body) : ''})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data :date[iso]'))
 app.use(express.static('build'))
 
 app.get('/info', (request, response, next) => {
   Person.count({})
-  .then((count) => {
-    response.send(`<p>Phonebook has info for ${count} people</p><p>${new Date}</p>`)
-  })
-  .catch(error => next(error))
+    .then((count) => {
+      response.send(`<p>Phonebook has info for ${count} people</p><p>${new Date}</p>`)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
-  .then(persons => {response.json(persons)})
-  .catch(error => next(error))
+    .then(persons => {response.json(persons)})
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person){
-      response.json(person)
-    }else {
-      response.status(404).send('No data found').end()
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person){
+        response.json(person)
+      }else {
+        response.status(404).send('No data found').end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 /*
@@ -41,8 +41,8 @@ app.post('/api/persons', (request, response, next)  => {
   const body = request.body
 
   if(!body.name || !body.number){
-    return response.status(400).json({ 
-        error: 'please provide the required info' 
+    return response.status(400).json({
+        error: 'please provide the required info'
       })
   }
 
@@ -50,7 +50,6 @@ app.post('/api/persons', (request, response, next)  => {
     name: body.name,
     number: body.number,
   }
-  
   Person.findOneAndUpdate({name: body.name}, person, {new:true, runValidators: true, context: 'query'})
   .then((updatedPerson) => {
     if(!updatedPerson){
@@ -76,9 +75,9 @@ app.post('/api/persons', (request, response, next)  => {
   const body = request.body
 
   if(!body.name || !body.number){
-    return response.status(400).json({ 
-        error: 'please provide the required info' 
-      })
+    return response.status(400).json({
+      error: 'please provide the required info'
+    })
   }
 
   const person = new Person({
@@ -87,10 +86,10 @@ app.post('/api/persons', (request, response, next)  => {
   })
 
   person.save()
-  .then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -99,34 +98,34 @@ app.put('/api/persons/:id', (request, response, next) => {
     name: body.name,
     number: body.number,
   }
-  Person.findByIdAndUpdate(request.params.id, person, {new:true, runValidators: true, context: 'query'})
-  .then((updatedPerson) => {
-    response.json(updatedPerson)
-  })
-  .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new:true, runValidators: true, context: 'query' })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id)
-  .then(() => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if(error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
